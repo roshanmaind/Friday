4b96a3b68f6e7e3f42c8bf6a227f66b7adb31b04
roshanmaind <roshanmaind3434@gmail.com>
2018-09-09 22:30:16 +0530

    sign in page layout complete

--- modules/friday/loginator.py
+++ modules/friday/loginator.py
@@ -1,69 +1,35 @@
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.boxlayout import BoxLayout
-from kivy.lang import Builder
 from kivy.config import Config
-
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 
 Config.set('graphics', 'width', '500')
 Config.set('graphics', 'height', '640')
 Config.set('graphics', 'resizable', False)
 
-root = Builder.load_string('''
-FloatLayout:
-    canvas.before:
-        Rectangle:
-            pos: self.pos
-            size: self.size
-            source: "../../data/loginator/res/bg.jpg"
-    Label:
-        color: 0.65, 0.65, 0.65, 0.6
-        font_name: "../../data/loginator/res/Pacifico.ttf"
-        text: "Welcome to Friday"
-        font_size: "25dp"
-        halign: 'center'
-        valign: 'middle'
-        pos_hint: {"x": 0, "y": 0.3}
-    Label:
-        color: 0.8, 0.8, 0.8, 1
-        font_name: "../../data/loginator/res/Roboto.ttf"
-        text: "or"
-        font_size: "19dp"
-        halign: 'center'
-        valign: 'middle'
-        pos_hint: {"x": 0, "y": -0.08}
-    Button:
-        background_normal: "../../data/loginator/res/button.png"
-        background_down: "../../data/loginator/res/buttond.png"
-        pos_hint: {"right": .75, "top": 0.5}
-        size_hint: (.5,.042)
-        Label:
-            font_name: "../../data/loginator/res/Lobster.otf"
-            font_size: "17dp"
-            center: self.parent.center
-            text: "Register"
-    Button:
-        background_normal: "../../data/loginator/res/button2.png"
-        background_down: "../../data/loginator/res/button2d.png"
-        pos_hint: {"right": .657, "top": 0.38}
-        size_hint: (.3,.042)
-        Label:
-            font_name: "../../data/loginator/res/Lobster.otf"
-            font_size: "17dp"
-            center: self.parent.center
-            text: "Sign in"
-''')
+class Manager(ScreenManager):
+    pass
+
+class Greet(Screen):
+    pass
+
+class Register(Screen):
+    pass
+
+class Signin(Screen):
+    pass
 
 class Login(App):
+    def __init__(self, user, **kwargs):
+        super(Login, self).__init__(**kwargs)
+        self.user = user
+
     def build(self):
-        return root
+        return Manager()
+
 
 def login(user):
-    Login().run()
+    Login(user).run()
     return user
 
-
-def twitter_link():
-    pass
 
 login(5)
