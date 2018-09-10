8f4c56d3a92d88af7089efe19ec09a37d7e6c2bf
roshanmaind <roshanmaind3434@gmail.com>
2018-09-11 03:52:33 +0530

    logging in

--- Friday.py
+++ Friday.py
@@ -8,9 +8,9 @@ import pickle
 if __name__ == "__main__":
     with open("data/friday/last_session.pkl", "rb") as file:
         user = pickle.load(file)
-    if not user["logged in"]:
+    if not user["logged_in"]:
         user = friday.loginator.login(user)
-        user["logged in"] = True
+        user["logged_in"] = True
     if not user["twitter_linked"]:
         user = twitter.linker.link(user)
         user["twitter_linked"] = True
--- modules/friday/loginator.py
+++ modules/friday/loginator.py
@@ -1,10 +1,14 @@
 from kivy.app import App
 from kivy.config import Config
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivy.properties import StringProperty, ObjectProperty
+import h5py
 
 Config.set('graphics', 'width', '500')
 Config.set('graphics', 'height', '640')
 Config.set('graphics', 'resizable', False)
+
+g_user = None
 
 class Manager(ScreenManager):
     pass
@@ -13,20 +17,57 @@ class Greet(Screen):
     pass
 
 class Register(Screen):
-    pass
+    user = StringProperty("")
+    passwd = StringProperty("")
+    def __init__(self, **kwargs):
+        super(Register, self).__init__(**kwargs)
+        with h5py.File("data/friday/users.hdf5", "r") as users_file:
+            database = users_file["users"]
+            database = list(database)
+            for i in range(len(database)):
+                database[i] = [attrib.decode("utf8") for attrib in database[i]]
+            self.database = database
+
+    def sign_up_click(self):
+        global g_user
 
 class Signin(Screen):
-    pass
+    user = StringProperty("")
+    passwd = StringProperty("")
+    message = StringProperty("")
+
+    def __init__(self, **kwargs):
+        super(Signin, self).__init__(**kwargs)
+        with h5py.File("data/friday/users.hdf5", "r") as users_file:
+            database = users_file["users"]
+            database = list(database)
+            for i in range(len(database)):
+                database[i] = [attrib.decode("utf8") for attrib in database[i]]
+            self.database = database
+
+    def sign_in_click(self):
+        global g_user
+        for entry in self.database:
+            if entry[2] == self.user and entry[3] == self.passwd:
+                g_user["first_name"] = entry[0]
+                g_user["last_name"] = entry[1]
+                g_user["username"] = self.user
+                g_user["twitter_linked"] = True if entry[4] == "Y" else False
+                App.get_running_app().stop()
+                return
+        self.message = "Incorrect username or password. Try again."
+        self.user = ""
+        self.passwd = ""
+
 
 class Login(App):
-    def __init__(self, user, **kwargs):
-        super(Login, self).__init__(**kwargs)
-        self.user = user
-
     def build(self):
         return Manager()
 
 
 def login(user):
-    Login(user).run()
-    return user
+    global g_user
+    g_user = user
+    Login().run()
+    print("Signed in as", g_user["first_name"], g_user["last_name"])
+    return g_user
