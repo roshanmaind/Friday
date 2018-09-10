eb1d40ed96fdc04d00df479ca1aa7b45d7578ecb
roshanmaind <roshanmaind3434@gmail.com>
2018-09-11 01:12:49 +0530

    seperate twitter module

--- Friday.py
+++ Friday.py
@@ -1,6 +1,6 @@
 # Python 3.6.5
 
-from modules.friday import loginator
+from modules import friday, twitter
 from os import system
 import pickle
 
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     with open("data/friday/user.pkl", "rb") as file:
         user = pickle.load(file)
     if not user["logged in"]:
-        user = loginator.login(user)
+        user = friday.loginator.login(user)
         user["logged in"] = True
     if not user["twitter_linked"]:
-        user = loginator.twitter_link(user)
+        user = twitter.linker.link(user)
         user["twitter_linked"] = True
