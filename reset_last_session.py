e304ee6a652f5b9cc709b80df738a40a5de41a58
roshanmaind <roshanmaind3434@gmail.com>
2018-09-16 04:23:15 +0530

    added root_path finder

--- reset_last_session.py
+++ reset_last_session.py
@@ -1,5 +1,9 @@
 import pickle
+import os
 
-with open("data/friday/last_session.pkl", "wb") as file:
-    user = {"logged_in": False, "first_name": "first", "last_name": "last", "username": "username", "twitter_linked": False}
-    pickle.dump(user, file)
+root_path = os.path.realpath(__file__)
+root_path = root_path[:len(root_path) - 21]
+
+with open(str(root_path + "data/friday/last_session.pkl"), "wb") as file:
+	user = {"logged_in": False, "first_name": "", "last_name": "", "username": "", "twitter_linked": False}
+	pickle.dump(user, file)
