760acd54b77dff29675639c0c6914a5ee8914769
roshanmaind <roshanmaind3434@gmail.com>
2018-09-16 04:22:47 +0530

    spaces to tabs

--- modules/friday/loginator.py
+++ modules/friday/loginator.py
@@ -3,145 +3,156 @@ from kivy.config import Config
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.properties import StringProperty, BooleanProperty
 import h5py
+import os
+
+
+root_path = os.path.realpath(__file__)
+root_path = root_path[:len(root_path)- 27]
+
 
 Config.set('graphics', 'width', '500')
 Config.set('graphics', 'height', '640')
 Config.set('graphics', 'resizable', False)
 
+
 g_user = None
 
+
 class Manager(ScreenManager):
-    pass
+	pass
+
 
 class Greet(Screen):
-    pass
+	pass
+
 
 class Register(Screen):
-    first = StringProperty("")
-    last = StringProperty("")
-    user = StringProperty("")
-    passwd = StringProperty("")
-    passwd2 = StringProperty("")
-    message = StringProperty("")
+	first = StringProperty("")
+	last = StringProperty("")
+	user = StringProperty("")
+	passwd = StringProperty("")
+	passwd2 = StringProperty("")
+	message = StringProperty("")
 
-    def __init__(self, **kwargs):
-        super(Register, self).__init__(**kwargs)
-        with h5py.File("data/friday/users.hdf5", "r") as users_file:
-            database = users_file["users"]
-            database = list(database)
-            for i in range(len(database)):
-                database[i] = [attrib.decode("utf8") for attrib in database[i]]
-            self.database = database
+	def __init__(self, **kwargs):
+		super(Register, self).__init__(**kwargs)
+		with h5py.File(str(root_path + "data/friday/users.hdf5"), "r") as users_file:
+			database = users_file["users"]
+			database = list(database)
+			for i in range(len(database)):
+				database[i] = [attrib.decode("utf8") for attrib in database[i]]
+			self.database = database
 
-    def sign_up_click(self):
-        global g_user
+	def sign_up_click(self):
+		global g_user
 
-        for entry in self.database:
-            if entry[2] == self.user:
-                self.message = "Username taken. Use something else."
-                self.first = ""
-                self.last = ""
-                self.user = ""
-                self.passwd = ""
-                self.passwd2 = ""
-                return
+		for entry in self.database:
+			if entry[2] == self.user:
+				self.message = "Username taken. Use something else."
+				self.first = ""
+				self.last = ""
+				self.user = ""
+				self.passwd = ""
+				self.passwd2 = ""
+				return
 
-        if self.passwd != self.passwd2:
-            self.message = "Passwords don't match."
-            self.first = ""
-            self.last = ""
-            self.user = ""
-            self.passwd = ""
-            self.passwd2 = ""
-            return
+		if self.passwd != self.passwd2:
+			self.message = "Passwords don't match."
+			self.first = ""
+			self.last = ""
+			self.user = ""
+			self.passwd = ""
+			self.passwd2 = ""
+			return
 
-        if self.user == "":
-            self.message = "Username can't be empty"
-            self.first = ""
-            self.last = ""
-            self.user = ""
-            self.passwd = ""
-            self.passwd2 = ""
-            return
+		if self.user == "":
+			self.message = "Username can't be empty"
+			self.first = ""
+			self.last = ""
+			self.user = ""
+			self.passwd = ""
+			self.passwd2 = ""
+			return
 
-        if self.first == "":
-            self.message = "First name can't be empty"
-            self.first = ""
-            self.last = ""
-            self.user = ""
-            self.passwd = ""
-            self.passwd2 = ""
-            return
+		if self.first == "":
+			self.message = "First name can't be empty"
+			self.first = ""
+			self.last = ""
+			self.user = ""
+			self.passwd = ""
+			self.passwd2 = ""
+			return
 
-        if self.last == "":
-            self.message = "Last name can't be empty"
-            self.first = ""
-            self.last = ""
-            self.user = ""
-            self.passwd = ""
-            self.passwd2 = ""
-            return
+		if self.last == "":
+			self.message = "Last name can't be empty"
+			self.first = ""
+			self.last = ""
+			self.user = ""
+			self.passwd = ""
+			self.passwd2 = ""
+			return
 
-        if self.passwd == "":
-            self.message = "Password can't be empty"
-            self.first = ""
-            self.last = ""
-            self.user = ""
-            self.passwd = ""
-            self.passwd2 = ""
-            return
+		if self.passwd == "":
+			self.message = "Password can't be empty"
+			self.first = ""
+			self.last = ""
+			self.user = ""
+			self.passwd = ""
+			self.passwd2 = ""
+			return
 
-        self.database.append([str(self.first), str(self.last), str(self.user), str(self.passwd), "N"])
-        for i in range(len(self.database)):
-            self.database[i] = [attrib.encode("utf8") for attrib in self.database[i]]
-        with h5py.File("data/friday/users.hdf5", "w") as users_file:
-            d = users_file.create_dataset("users", data=self.database)
-        self.message = "Account created! Restart Friday to login."
-        self.first = ""
-        self.last = ""
-        self.user = ""
-        self.passwd = ""
-        self.passwd2 = ""
+		self.database.append([str(self.first), str(self.last), str(self.user), str(self.passwd), "N"])
+		for i in range(len(self.database)):
+			self.database[i] = [attrib.encode("utf8") for attrib in self.database[i]]
+		with h5py.File(str(root_path + "data/friday/users.hdf5"), "w") as users_file:
+			d = users_file.create_dataset("users", data=self.database)
+		self.message = "Account created! Restart Friday to login."
+		self.first = ""
+		self.last = ""
+		self.user = ""
+		self.passwd = ""
+		self.passwd2 = ""
+
 
 class Signin(Screen):
-    user = StringProperty("")
-    passwd = StringProperty("")
-    message = StringProperty("")
-    remember = BooleanProperty(True)
+	user = StringProperty("")
+	passwd = StringProperty("")
+	message = StringProperty("")
+	remember = BooleanProperty(True)
 
-    def __init__(self, **kwargs):
-        super(Signin, self).__init__(**kwargs)
-        with h5py.File("data/friday/users.hdf5", "r") as users_file:
-            database = users_file["users"]
-            database = list(database)
-            for i in range(len(database)):
-                database[i] = [attrib.decode("utf8") for attrib in database[i]]
-            self.database = database
-        self.remember = True
+	def __init__(self, **kwargs):
+		super(Signin, self).__init__(**kwargs)
+		with h5py.File(str(root_path + "data/friday/users.hdf5"), "r") as users_file:
+			database = users_file["users"]
+			database = list(database)
+			for i in range(len(database)):
+				database[i] = [attrib.decode("utf8") for attrib in database[i]]
+			self.database = database
+		self.remember = True
 
-    def sign_in_click(self):
-        global g_user
-        for entry in self.database:
-            if entry[2] == self.user and entry[3] == self.passwd:
-                g_user["first_name"] = entry[0]
-                g_user["last_name"] = entry[1]
-                g_user["username"] = self.user
-                g_user["logged_in"] = True if self.remember else False
-                g_user["twitter_linked"] = True if entry[4] == "Y" else False
-                App.get_running_app().stop()
-                return
-        self.message = "Incorrect username or password. Try again."
-        self.user = ""
-        self.passwd = ""
+	def sign_in_click(self):
+		global g_user
+		for entry in self.database:
+			if entry[2] == self.user and entry[3] == self.passwd:
+				g_user["first_name"] = entry[0]
+				g_user["last_name"] = entry[1]
+				g_user["username"] = self.user
+				g_user["logged_in"] = True if self.remember else False
+				g_user["twitter_linked"] = True if entry[4] == "Y" else False
+				App.get_running_app().stop()
+				return
+		self.message = "Incorrect username or password. Try again."
+		self.user = ""
+		self.passwd = ""
 
 
 class Login(App):
-    def build(self):
-        return Manager()
+	def build(self):
+		return Manager()
 
 
 def login(user):
-    global g_user
-    g_user = user
-    Login().run()
-    return g_user
+	global g_user
+	g_user = user
+	Login().run()
+	return g_user
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys
 from os import system
 
 if sys.platform == "linux":
-    print('''
+	print('''
 Found linux OS. Assuming a Debian based distro...
 
 You may be prompted to enter sudo (root user) password
@@ -16,20 +16,20 @@ This setup will install on your machine the following softwares and packages:-
 The softwares or packages that already exist will be skipped.
 
 Do you wish to continue? (Y/N):''', end=" ")
-    choice = input()
-    if choice.upper() != "Y":
-        exit()
-    print("\nInitiating OpenGL installation...")
-    system("sudo apt-get install mesa-utils freeglut3 freeglut3-dev")
+	choice = input()
+	if choice.upper() != "Y":
+		exit()
+	print("\nInitiating OpenGL installation...")
+	system("sudo apt-get install mesa-utils freeglut3 freeglut3-dev")
 
-    print("\nInitiating python3-pip installation...")
-    system("sudo apt install python3-pip")
+	print("\nInitiating python3-pip installation...")
+	system("sudo apt install python3-pip")
 
-    print("\nInstalling required python3 modules...")
-    system("pip3 install -r requirements.txt")
+	print("\nInstalling required python3 modules...")
+	system("pip3 install -r requirements.txt")
 
 else:
-    print('''
+	print('''
 We are sorry. Friday does not support any other OS than a Debian based Linux distro as of now.
 Support for other operating systems will soon be available! Stay tuned.
 You may check the github repository of Friday on github.com/roshanmaind/Friday and raise
