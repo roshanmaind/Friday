048dbefcca828d655757f09d3251a811dedc2765
roshanmaind <roshanmaind3434@gmail.com>
2018-09-16 04:16:45 +0530

    watson tone analyzer complete

---
+++ modules/watson/__init__.py
@@ -0,0 +1,1 @@
+from modules.watson import analyze
---
+++ modules/watson/analyzer.py
@@ -0,0 +1,39 @@
+from watson_developer_cloud import ToneAnalyzerV3
+
+
+watson = ToneAnalyzerV3(
+            username="ff8b4cfa-542c-4ff2-b7f6-02073439402b",
+            password="mBJehlG1CF1x",
+            version="2017-09-26"
+         )
+
+
+'''Analyze the tone of the tweets in the user dictionary.
+
+The tweets received by the main script will be strings in a list. 
+The funciton joins all the tweets in one string in which each tweet will be enclosed
+within opening and closing <p> html tags. Thus, the function calls the watson tone 
+analyzer api with content_type="text/html" argument.
+'''
+def analyze(user):
+	global watson
+
+	tweets = user["tweets"]
+	tone_input = ""
+	for tweet in tweets:
+		tone_input = tone_input + " <p> " + tweet + " </p> "
+	user["tone"] = watson.tone(tone_input, content_type='text/html')
+	return user
+
+
+if __name__ == "__main__":
+	user = {
+					"tweets": 
+						[
+							"Hey! This is my first tweet!", 
+							"Loving Twitter so far! I just know I'm gonna get addicted to this thing in no time...", 
+							"GOD, I can't wait for the new season of Silicon Valley"
+						]
+				}
+
+	user = analyze(user)
