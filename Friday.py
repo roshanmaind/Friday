a32c5348b7f99ab01a44eb825ada1729912947d4
roshanmaind <roshanmaind3434@gmail.com>
2018-09-16 04:23:40 +0530

    complete flow

--- Friday.py
+++ Friday.py
@@ -1,21 +1,48 @@
-# Python 3.6.5
-
-from modules import friday, twitter
-from os import system
+from modules import friday, twitter, watson
+import os
 import pickle
 
 
+root_path = os.path.realpath(__file__)
+root_path = root_path[:len(root_path) - 9]
+
+
+def load_session():
+	with open(str(root_path + "data/friday/last_session.pkl"), "rb") as file:
+		user = pickle.load(file)
+	return user
+
+
+def end_session(user):
+	# clean user information before saving if the user doesn't want to be remembered
+	if not user["logged_in"]:
+		user["first_name"] = ""
+		user["last_name"] = ""
+		user["username"] = ""
+
+	with open(str(root_path + "data/friday/last_session.pkl"), "wb") as file:
+		pickle.dump(user, file)
+
+
+def check_login(user):
+	if not user["logged_in"]:
+		user = friday.loginator.login(user)
+		if user["first_name"] == "" and user["last_name"] == "":
+			print("Bye!")
+			exit()
+		print("Signed in as", user["first_name"], user["last_name"])
+
+
+def main():
+	user = load_session()
+	check_login(user)
+	
+	user = twitter.tweets.get_tweets(user)
+	user = watson.analyzer.analyze(user)
+
+	friday.main.run(user)
+
+	end_session(user)
+
 if __name__ == "__main__":
-    with open("data/friday/last_session.pkl", "rb") as file:
-        user = pickle.load(file)
-    if not user["logged_in"]:
-        user = friday.loginator.login(user)
-        if user["first_name"] == "first" and user["last_name"] == "last":
-            print("Bye!")
-            exit()
-        print("Signed in as", user["first_name"], user["last_name"])
-    if not user["twitter_linked"]:
-        user = twitter.linker.link(user)
-        user["twitter_linked"] = True
-    with open("data/friday/last_session.pkl", "wb") as file:
-        pickle.dump(user, file)
+	main()
