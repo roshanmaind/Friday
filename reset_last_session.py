6120a9a84fec749027cf6665943a9ffd1d546c5f
roshanmaind <roshanmaind3434@gmail.com>
2018-09-19 22:51:46 +0530

    rename

--- reset_last_session.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import os
-
-root_path = os.path.realpath(__file__)
-root_path = root_path[:len(root_path) - 21]
-
-with open(str(root_path + "data/friday/last_session.pkl"), "wb") as file:
-	user = {"logged_in": False, "first_name": "", "last_name": "", "username": "", "twitter_linked": False}
-	pickle.dump(user, file)
