ffc52f04a9cdcae57e1fc7061dc56a51ba977cc2
roshanmaind <roshanmaind3434@gmail.com>
2018-09-21 19:44:37 +0530

    unnecessary imports

--- dev_tools/songs_archive_manager/billboard_fetch.py
+++ dev_tools/songs_archive_manager/billboard_fetch.py
@@ -1,7 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import re
 from sys import argv
