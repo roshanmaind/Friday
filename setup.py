4e4bf866a101cf85eaa1aa46271754baf3d5a9f3
roshanfmaind <roshanmaind3434@gmail.com>
2018-09-28 00:31:22 +0530

    Windows

--- setup.py
+++ setup.py
@@ -3,9 +3,13 @@ from os import system
 from os import getcwd
 
 if sys.platform == "linux":
-	print('''
-Found linux OS. Assuming a Debian based distro...
+	print("Found a Linux Operating System")
+	print("This setup script only supports Debian based distros of Linux as of now.")
+	print("Enter 'y' to continue if you're using a Debian based distro ('n' to exit): ", end="")
+	if input().upper() != "Y":
+		exit()
 
+	print('''
 You may be prompted to enter sudo (root user) password
 This setup will install on your machine the following softwares and packages:-
 1. OpenGL
@@ -16,13 +20,13 @@ This setup will install on your machine the following softwares and packages:-
 6. h5py
 7. python-twitter
 8. Selenium
-9. geckodriver
+9. requests_oauthlib
+10. geckodriver
 
 The softwares or packages that already exist will be skipped.
 
 Do you wish to continue? (Y/N):''', end=" ")
-	choice = input()
-	if choice.upper() != "Y":
+	if input().upper() != "Y":
 		exit()
 	print("\nInitiating OpenGL installation...")
 	system("sudo apt-get install mesa-utils freeglut3 freeglut3-dev")
@@ -47,11 +51,36 @@ Do you wish to continue? (Y/N):''', end=" ")
 
 
 
+elif sys.platform[:3] == "win":
+	print('''
+Windows OS detected...
+
+This setup will install on your machine the following modules:-
+1. watson developer cloud
+2. cython
+3. kivy
+4. h5py
+5. python-twitter
+6. Selenium
+7. requests_oauthlib
+
+The modules that already exist will be skipped.
+
+Do you wish to continue? (Y/N):''', end=" ")
+	choice = input()
+	if choice.upper() != "Y":
+		exit()
+
+	print("\nInstalling required python3 modules...")
+	system("pip3 install -r requirements.txt")
+
+	print("Modules installation finished.")
+
 
 else:
 	print('''
-We are sorry. Friday does not support any other OS than a Debian based Linux distro as of now.
-Support for other operating systems will soon be available! Stay tuned.
+We are sorry. Friday does not support any other OS than a Debian based Linux distro and Windows 
+right now. Support for other operating systems will soon be available! Stay tuned.
 You may check the github repository of Friday on github.com/roshanmaind/Friday and raise
 an issue requesting other OS support if there is no existing similar issue on the repository.
 
